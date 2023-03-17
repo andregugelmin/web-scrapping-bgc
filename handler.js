f42d627c83923fe6cfa801a5d8a6326faa4a769a
andregugelmin <andregugelmin1996@gmail.com>
2023-03-17 14:07:10 -0300

    function getProducts access amazon website and get a list of links in the div where the productsare

--- handler.js
+++ handler.js
@@ -1,15 +1,22 @@
 'use strict';
-
 const puppeteer = require('puppeteer');
 
 module.exports.getProducts = async (event, context, callback) => {
 	const browser = await puppeteer.launch({
 		headless: false,
 	});
+
 	const page = await browser.newPage();
 	await page.setViewport({ width: 1920, height: 1080 });
-	await page.goto('https://www.amazon.com.br');
-	await page.screenshot({ path: 'amazon.png' });
+	await page.goto('https://www.amazon.com.br/gp/bestsellers/kitchen');
+	const links = await page.$$eval('#zg a img', (images) => {
+		return images.map((img) => {
+			const link = img.closest('a');
+			return link.href;
+		});
+	});
 
-	await browser.close();
+	console.log(links);
+
+	//await browser.close();
 };
