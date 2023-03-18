2909c83fd9a0de110da9c6f12ec47263e272247a
andregugelmin <andregugelmin1996@gmail.com>
2023-03-18 11:20:29 -0300

    function now gets title, price, link and image of the first 3 amazon itens bestseller and returnin a lambda function

--- handler.js
+++ handler.js
@@ -2,47 +2,41 @@
 const puppeteer = require('puppeteer');
 
 module.exports.getProducts = async (event, context, callback) => {
-	const browser = await puppeteer.launch({});
+	const amazonItens = await getAmazonItens();
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ itens: amazonItens }),
+	};
+};
+
+async function getAmazonItens() {
+	const browser = await puppeteer.launch({
+		headless: false,
+	});
 
 	const page = await browser.newPage();
 	await page.setViewport({ width: 1920, height: 1080 });
-	await page.goto('https://www.amazon.com.br/gp/bestsellers/kitchen/');
+	await page.goto('https://www.amazon.com.br/gp/bestsellers/home/');
 
-	// const productsHandle = await page.$$('div.a-cardui');
-	// const objArray = [];
-
-	// for (const product of productsHandle) {
-	// 	const itemObj = {};
-	// 	try {
-	// 		const title = await page.evaluate(
-	// 			(elem) => elem.querySelector('a:nth-child(2) > span > div ').textContent,
-	// 			product
-	// 		);
-	// 		itemObj.title = title;
-	// 	} catch (error) {}
-	// }
-	const allLinks = await page.$$eval('#zg a img', (images) => {
-		return images.map((img) => {
-			const link = img.closest('a');
-			return link.href;
-		});
-	});
-
-	const links = allLinks.slice(0, 3);
 	const productsArray = [];
-
-	for (const link of links) {
-		const productObj = { link };
+	for (let i = 0; i < 3; i++) {
+		const productObj = {};
 		try {
-			await page.goto(link);
-			const title = await page.$eval('#ppd h1 span', ({ textContent }) => textContent);
-			const price = await page.$eval('#corePrice_feature_div .a-price .a-offscreen', ({ textContent }) => textContent);
+			const image = await page.$eval(`#p13n-asin-index-${i} img`, ({ src }) => src);
+			const link = await page.$eval(`#p13n-asin-index-${i} a`, ({ href }) => href);
+			const title = await page.$eval(
+				`#p13n-asin-index-${i} a:nth-child(2) > span > div`,
+				({ textContent }) => textContent
+			);
+			const price = await page.$eval(`#p13n-asin-index-${i} .a-color-price span`, ({ textContent }) => textContent);
 			productObj.title = title.trim();
+			productObj.image = image.trim();
+			productObj.link = link.trim();
 			productObj.price = price.trim();
 			productsArray.push(productObj);
-			//console.log(title.trim(), price.trim());
 		} catch (error) {}
 	}
-	console.log(productsArray);
+
 	await browser.close();
-};
+	return productsArray;
+}
